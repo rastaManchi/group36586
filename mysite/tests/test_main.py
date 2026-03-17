7e80be8f009e939911cd47facfceae0ca3b6dd3c
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2026-03-17 16:37:28 +0300

    Update

---
+++ mysite/tests/test_main.py
@@ -0,0 +1,17 @@
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create_user('user', 'user@mail.ru', 'password')
+    assert User.objects.count() == 1
+    
+    
+@pytest.mark.django_db
+def test_view(admin_client):
+    # url = reverse('')
+    response = admin_client.get('http://localhost:8000/home/')
+    print(response.content)
+    assert response.status_code == 200
